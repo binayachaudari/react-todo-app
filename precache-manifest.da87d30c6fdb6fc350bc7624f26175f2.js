f026c414d885b9e4fd32907267fa88c5f81b89b0
Binaya Chaudhary <binayachaudari@live.com>
2019-08-10 17:56:01 +0545

    Updates

---
+++ precache-manifest.da87d30c6fdb6fc350bc7624f26175f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213feef4fe4e293be1a8f89a1de710a7",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "fcbbd8472a771286396b",
+    "url": "/react-todo-app/static/css/main.2e5ab63d.chunk.css"
+  },
+  {
+    "revision": "76c0a88404afa84fee43",
+    "url": "/react-todo-app/static/js/2.1ae45d03.chunk.js"
+  },
+  {
+    "revision": "fcbbd8472a771286396b",
+    "url": "/react-todo-app/static/js/main.20ce8650.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
